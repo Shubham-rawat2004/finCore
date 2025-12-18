ad3f0fe37ba4a43aa71533cc4186b2d27f728162
Shubham Rawat <shubhamrawat1808@gmail.com>
2025-12-19 03:38:03 +0530

    Initial commit

---
+++ src/test/java/com/rawat/FinCore/FinCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawat.FinCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
